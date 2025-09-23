30c70220eab2059dd3e890fe63eec20bc0460cb1
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2025-09-23 19:03:21 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // bật strict mode để debug
+  reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com", "avatars.githubusercontent.com"],
     formats: ["image/avif", "image/webp"],
   },
   experimental: {
-    optimizeCss: true, // tối ưu CSS
-    // serverActions: {}, // chỉ bật nếu bạn thật sự dùng Server Actions
+    optimizeCss: true,
   },
-  output: "export", // build ra static HTML (frontend only)
 };
 
 export default nextConfig;
